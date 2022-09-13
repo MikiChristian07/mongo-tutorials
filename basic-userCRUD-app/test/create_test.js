c3d4a20575dd2997b17aa59312e22cef4fed4952
Christian <onovochristian19@gmail.com>
2022-09-13 10:08:35 +0100

    =Integration ES6 promises for deprecation errors and upgraded the mondogb connection with before

--- basic-userCRUD-app/test/create_test.js
+++ basic-userCRUD-app/test/create_test.js
@@ -2,10 +2,14 @@ import assert from 'assert';
 import User from '../src/user.js';
 
 describe('Creating records', () => {
-    it('saves a user', () => {
+    it('saves a user', (done) => {
         const mike = new User({ name:  'Mike' });
 
         mike.save()
+            .then((success) => {
+                 assert(!mike.isNew);
+                 done();
+            })
     });
 
 });
--- basic-userCRUD-app/test/test_helper.js
+++ basic-userCRUD-app/test/test_helper.js
@@ -1,8 +1,21 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://localhost/users_test');
-mongoose.connection
-    .once('open', () => console.log('Connected to the database...'))
-    .on('error', (error) => {
-        console.warn('Warning', error);        
-    })
+mongoose.Promise = global.Promise;
+
+before((done) => {
+    mongoose.connect('mongodb://localhost/users_test');
+    mongoose.connection
+        .once('open', () => { done(); })
+        .on('error', (error) => {
+            console.warn('Warning', error);        
+        });
+});
+
+beforeEach((done) => {
+    mongoose.connection.collections.users.drop(() => {
+    // calling done to go to the next code piece 
+        done();
+    });
+});
+
+ 
